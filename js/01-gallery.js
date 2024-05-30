44af908b169d2377c1a1153da080464f1ec00d53
Manon4ik <manon4ik@gmail.com>
2024-05-30 03:39:05 +0300

    first commit

---
+++ js/01-gallery.js
@@ -0,0 +1,22 @@
+import { galleryItems } from "./gallery-items.js";
+
+import SimpleLightbox  from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = galleryItems.map( ({ preview, original, description }) =>
+  `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+).join("")
+
+const gallery = document.getElementById("gallery");
+gallery.innerHTML = galleryList;
+
+let lightbox = new SimpleLightbox('.gallery a', 
+    { 
+        captionsData: 'alt',
+        captionDelay: 250
+    }
+);
